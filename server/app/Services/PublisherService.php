aba4b7097d7d9844d615df3c044282b97d86b6b3
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-03-28 01:14:57 +0900

    test eloquent

---
+++ server/app/Services/PublisherService.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\DataProvider\Eloquent\Publisher;
+
+class PublisherService
+{
+    public function exisits(string $name): bool
+    {
+        $count = Publisher::whereName($name)->count();
+        if ($count > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public function store(string $name, string $address): int
+    {
+        $publisher = Publisher::create(
+            [
+                'name' => $name,
+                'address' => $address
+            ]
+        );
+        return (int)$publisher->id;
+    }
+
+    public function get(int $id)
+    {
+        return Publisher::find($id);
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('publishers', [
+    App\Http\Controllers\PublisherAction::class,
+    'create',
+]);
+
+Route::get('publishers', [
+    App\Http\Controllers\PublisherAction::class,
+    'get',
+]);
